caee699135bfeb2462dc9547608dbb8f4ea5932f
James <tikx.batera@gmail.com>
2020-11-19 11:07:30 -0300

    Adding support to i18n-and-l10n with Flask-Babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 app = Flask(__name__)
@@ -16,9 +17,17 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -47,5 +56,6 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
 
 from app import routes, models, errors  # nopep8
--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config (object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
